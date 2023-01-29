195006fb033c52cc2d05c6b873dff5801a57ce29
thomasfevre <thomas.fevre@edu.devinci.fr>
2023-01-29 21:25:54 +0100

    some details

--- src/components/home.js
+++ src/components/home.js
@@ -8,8 +8,8 @@ export function HomePage() {
       document.querySelectorAll(".object").forEach(function(move){
 
         var moving_value = move.getAttribute("data-value");
-        var x = (e.clientX * moving_value) / 250;
-        var y = (e.clientY * moving_value) / 250;
+        var x = (e.clientX * moving_value) / 200;
+        var y = (e.clientY * moving_value) / 200;
 
         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
       });
@@ -31,7 +31,7 @@ export function HomePage() {
               <h2 className="object" data-value="3" >Password Manager</h2>
               
               <button className="btn-scroll object" data-value="4" onClick={handleClickScroll}>
-                <img src={require("../images/down-arrow2.png")} alt="down arrows" width='auto' height='auto'/>
+                Get Started
               </button>
             <img src={require("../images/1.png")} className="object" data-value="-2" alt=""/>
             <img src={require("../images/2.png")} className="object" data-value="6" alt=""/>
@@ -43,12 +43,28 @@ export function HomePage() {
             <img src={require("../images/8.png")} className="object" data-value="-9" alt=""/>
             <img src={require("../images/9.png")} className="object" data-value="-5" alt=""/>
         </div>
-        <div className="page-1" id="page-1"> 
-        <button class="cssbuttons-io-button"> Get started
-          <div class="icon">
-            <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+        <div className="page-1" id="page-1">
+          <div className="flex">
+            <div class="card">
+              <div class="card2">
+              </div>
+            </div>
+            <div className="m-10"></div>
+            <div class="card">
+              <div class="card2">
+              </div>
+            </div>
+            <div className="m-10"></div>
+            <div class="card">
+              <div class="card2">
+              </div>
+            </div>
           </div>
-        </button>
+          <button class="cssbuttons-io-button">Launch
+            <div class="icon">
+              <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+            </div>
+          </button>
 
         </div>
         </>
