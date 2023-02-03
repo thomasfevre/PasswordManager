c54b38eab4a9bc2f4e6a954cb4d6b977886d245d
thomasfevre <thomas.fevre@edu.devinci.fr>
2023-02-03 22:32:24 +0100

    front v1 done

--- src/components/home.js
+++ src/components/home.js
@@ -56,7 +56,7 @@ export function HomePage() {
             <h1>What's the project ?
               <svg className="animate-bounce mt-2" viewBox="0 0 1024 1024"  version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z" fill="#FFFFFF"></path></g></svg>            </h1>
             <p>
-              The objective of this project is to provide a password manager using the blockchain. By encrypting their passwords with their wallet (their public key), everyone can save their confidential data on the blockchain without the risk of theft or forgetting.
+              The purpose of this project is to provide a password manager using blockchain. By encrypting their passwords with their wallet (their public key), everyone can save their confidential data on the blockchain without the risk of theft or forgetting.
             </p><br/>
             <p className="italic">This project is a proof of concept, and should not be considered as a real tool</p>
           </div>
@@ -65,6 +65,7 @@ export function HomePage() {
               <div className="card2">
                 <h2>Easy</h2>
                 <img className="-mt-6" src={require("../images/wallet2.png")} alt=""/>
+                <p className="card-text -mt-6">Connect with your web3 wallet</p>
               </div>
             </div>
             <div className="m-10"></div>
@@ -72,6 +73,7 @@ export function HomePage() {
               <div className="card2">
                 <h2>Safe</h2>
                 <img src={require("../images/lock.png")} alt=""/>
+                <p className="card-text">Encrypt your data with your wallet</p>
               </div>
             </div>
             <div className="m-10"></div>
@@ -79,14 +81,17 @@ export function HomePage() {
               <div className="card2">
                 <h2>Decentralized</h2>
                 <img className="mx-12 my-6" src={require("../images/blockchain.png")} width={100} alt=""/>
+                <p className="card-text">Access it anywhere through the blockchain</p>
               </div>
             </div>
           </div>
-          <button className="cssbuttons-io-button">Launch
-            <div className="icon">
-              <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
-            </div>
-          </button>
+          <Link to="/main">
+            <button className="cssbuttons-io-button">Launch
+              <div className="icon">
+                <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+              </div>
+            </button>
+          </Link>
           <Footer />
         </div>
         
